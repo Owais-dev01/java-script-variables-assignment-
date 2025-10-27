0624bf1974851ad5979acb2bbfe4ee9498856281
Muhammad Owais Danish <owaisdaanish@gmail.com>
2025-10-27 03:14:47 -0700

    first

---
+++ Javascript/chap 1-4/app.js
@@ -0,0 +1,39 @@
+
+// Chapter 2: Variables and Alerts
+var username;
+var myName = "Muhammad Owais Danish";
+var message;
+message = "Hello World";
+alert(message);
+var studentName = "Ali Khan";
+var studentAge = "20 years old";
+var studentCourse = "Certified JavaScript Developer";
+alert(studentName);
+alert(studentAge);
+alert(studentCourse);
+var email;
+email = "owaisdaanish@gmail.com";
+alert("My Email Address is " + email);
+var book;
+book = "A smarter way to learn JavaScript";
+alert("I am Trying to learn JavaScript " + book);
+document.write("Yah! I can Write HTML content through JavaScript<br>");
+alert("▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬");
+
+// Chapter 3: Variables for Numbers
+var age;
+age = "15";
+alert("I am " + age + " Years Old");
+// Chapter 4: Rules for Naming Variables
+var a = 10, b = 20, c = 30;
+var name = "Ali";
+var $price = 100;
+var _total = 500;
+var user1 = "John";
+var myVariable = true;
+document.write("<b>Variable names can only contain letters, numbers, $ and _</b><br>");
+document.write("For example: $my_1stVariable<br><br>");
+document.write("<b>Variables must begin with a letter, $ or _</b><br>");
+document.write("For example: $name, _name or name<br><br>");
+document.write("<b>Variable names are case sensitive</b><br><br>");
+document.write("<b>Variable names should not be JS reserved keywords</b>");
